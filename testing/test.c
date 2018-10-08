46ca75d780f8f7ad168774c053a288ef01d74657
Mikayla Fischler <mafischler@wpi.edu>
2018-10-08 19:30:48 -0400

    testing code

--- testing/test.c
+++ testing/test.c
@@ -3,8 +3,19 @@
 #include <inttypes.h>
 
 uint8_t packet_malformed(uint8_t packet) {
-	//		validate blank			validate mode				validate color				validate running
-	return (packet & 0x40) || !((packet & 0x30) ^ 0x30) || !((packet & 0xE) ^ 0xE) || ((packet & 0x3E) && ~(packet & 0x1));
+	return
+		// validate connection/enable bit
+		((~packet & 0x80) 	&&  (packet & 0x7F)) ||
+		// validate running bit
+		((~packet & 0x1)	&&  (packet & 0x7E)) ||
+		// validate color uniqueness
+		(((packet & 0x4) >> 1)	& (packet & 0x2)) ||
+		(((packet & 0x8) >> 2)	& (packet & 0x2)) ||
+		(((packet & 0x8) >> 1)	& (packet & 0x4)) ||
+		// validate state
+		(((packet & 0x20) >> 1) & (packet & 0x10)) ||
+		(((packet & 0x40) >> 2) & (packet & 0x10)) ||
+		(((packet & 0x40) >> 1) & (~packet & 0x20));
 }
 
 #define ST_MODE_NOCOMMS_IDLE	0x00
@@ -19,6 +30,13 @@ uint8_t packet_malformed(uint8_t packet) {
 #define ST_MODE_PLACE_YELLOW	0xA3
 #define ST_MODE_PLACE_GREEN		0xA5
 #define ST_MODE_PLACE_BLUE		0xA9
+#define ST_MODE_PLACE_H_YELLOW	0xE3
+#define ST_MODE_PLACE_H_GREEN	0xE5
+#define ST_MODE_PLACE_H_BLUE	0xE9
+
+#define INVALID_1				0x79
+#define INVALID_2				0x8D
+#define INVALID_3				0xF5
 
 int main() {
 	printf("%d\n", packet_malformed(ST_MODE_NOCOMMS_IDLE));
@@ -33,6 +51,13 @@ int main() {
 	printf("%d\n", packet_malformed(ST_MODE_PLACE_YELLOW));
 	printf("%d\n", packet_malformed(ST_MODE_PLACE_GREEN));
 	printf("%d\n", packet_malformed(ST_MODE_PLACE_BLUE));
+	printf("%d\n", packet_malformed(ST_MODE_PLACE_H_YELLOW));
+	printf("%d\n", packet_malformed(ST_MODE_PLACE_H_GREEN));
+	printf("%d\n", packet_malformed(ST_MODE_PLACE_H_BLUE));
+
+	printf("%d\n", packet_malformed(INVALID_1));
+	printf("%d\n", packet_malformed(INVALID_2));
+	printf("%d\n", packet_malformed(INVALID_3));
 
 	return 0;
 }
